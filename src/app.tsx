3703ca6ab0f1be459e472c15a9de6e68f0b4e2bf
Mostafizur Rahman <mostafizurhimself@gmail.com>
2024-12-08 20:28:52 +0600

    adding typescript

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Content from "./components/content";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./app";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
